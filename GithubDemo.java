3f964e990f558b3e719b3ac03ae42b5ff257a99a
pradeeploganathan1818@gmail.com <pradeeploganathan1818@gmail.com>
2024-03-10 17:18:20 +0530

    updated-file

--- GithubDemo.java
+++ GithubDemo.java
@@ -1,7 +1,18 @@
 class GithubDemo
 {
+
+
+    public void print()
+    {
+        System.out.println("Hello Github");
+    }
+
 public static void main(String args[])
 {
 System.out.println("GitHub Demo");
+
+GithubDemo github = new GithubDemo();
+github.print();
+
 }
 }
